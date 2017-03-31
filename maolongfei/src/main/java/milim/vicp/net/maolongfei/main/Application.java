16271618db32315439ef9d04c705a79efeb362f4
Administrator <Administrator@PC-20151111PKXG>
2017-03-31 19:02:11 +0800

    将启动类放到个人包下面

--- maolongfei/src/main/java/milim/vicp/net/maolongfei/main/Application.java
+++ maolongfei/src/main/java/milim/vicp/net/maolongfei/main/Application.java
@@ -1,5 +1,7 @@
+package milim.vicp.net.maolongfei.main;
 import org.springframework.boot.SpringApplication;
-@EnableWebMvc
+import org.springframework.boot.SpringBootConfiguration;
+@SpringBootConfiguration
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
